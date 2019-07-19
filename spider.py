9fa0ba97b2f7107602c6f30186a639a22f7e4b36
matheuspessoa <matheus.pessoa16@gmail.com>
2019-07-19 10:03:39 -0300

    deploy no heroku

--- spider.py
+++ spider.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from flask import Flask, request, Response, abort, render_template, jsonify
 from flask_restful import Resource
+import os
+
 
 app = Flask(__name__)
 
@@ -47,4 +49,7 @@ def index(cnpj):
     
 
 if __name__ == '__main__':
-  app.run(debug=True)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
+
+  #app.run(debug=True)
