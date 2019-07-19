63f8fbf430b784943b898d47265e3564bf8841ae
matheuspessoa <matheus.pessoa16@gmail.com>
2019-07-19 09:46:34 -0300

    python webscraper

---
+++ spider.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from flask import Flask, request, Response, abort, render_template, jsonify
+from flask_restful import Resource
+
+app = Flask(__name__)
+
+
+url = 'http://cnpj.info/'
+
+teste = "24.365.710/0001-83"
+
+
+# def getInfo(cnpj):
+#     browser = webdriver.Firefox()
+#     try:
+#         browser.get(url)
+#         t = browser.find_element_by_xpath('//input[@type="text"]')
+#         t.send_keys(cnpj)
+#         b = browser.find_element_by_xpath("//input[@type='submit']").click()
+#         element = browser.find_element_by_tag_name("li")
+#         print(element.text)
+#         return element.text
+#     finally:
+#         browser.quit()
+
+# @app.route('/', methods=['GET', 'POST'])
+@app.route('/index/<cnpj>', methods=['GET'])
+def index(cnpj):
+    
+    if request.method == 'GET':
+        value = ''
+        browser = webdriver.Firefox()
+        try:
+            browser.get(url)
+            t = browser.find_element_by_xpath('//input[@type="text"]')
+            t.send_keys(cnpj)
+            b = browser.find_element_by_xpath("//input[@type='submit']").click()
+            element = browser.find_element_by_tag_name("li")
+            print(element.text)
+            # return render_template('index.html', title='Home', result=element.text)
+            # return {'status': 'success', 'data': element.text}, 200
+            return jsonify(element.text)
+        finally:
+            browser.quit()
+
+    
+
+if __name__ == '__main__':
+  app.run(debug=True)
